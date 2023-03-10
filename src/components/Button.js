a4e31484c1b68a0c0821e46a863afdd73aebc5d3
yunhwan <sky10240107@naver.com>
2023-03-10 15:59:34 +0900

    feat: Header,Button 컴포넌트 구현

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ img_path }) => {
+  <button className="Button"></button>;
+};
+
+export default Button;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
+
 const Home = () => {
   return (
-    <>
-      <div>1</div>
-    </>
+    <div className="Home">
+      <Header headText={"안녕하세요"} />
+    </div>
   );
 };
 
