5be3c4c3dee3f202336afdccd7897a953a190343
yunhwan <sky10240107@naver.com>
2023-04-04 21:32:33 +0900

    feat: 헤더 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
+import styled from "styled-components";
+
+const CustomHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10vh;
+  font-size: 30px;
+  font-weight: bolder;
+  background-color: white;
+`;
+
+const HeadBtnLeft = styled.div``;
+
+const HeadText = styled.div``;
+
+const HeadBtnRight = styled.div``;
+
 const Header = ({ headText, leftChild, rightChild }) => {
   return (
-    <header>
-      <div className="head_btn_left">{leftChild}</div>
-      <div className="head_text">{headText}</div>
-      <div className="head_btn_right">{rightChild}</div>
-    </header>
+    <CustomHeader>
+      <HeadBtnLeft>{leftChild}</HeadBtnLeft>
+      <HeadText>{headText}</HeadText>
+      <HeadBtnRight>{rightChild}</HeadBtnRight>
+    </CustomHeader>
   );
 };
 
