372cf41224c889508d08a766362cbe19ffbea92e
yunhwan <sky10240107@naver.com>
2023-04-04 21:32:00 +0900

    feat: 버튼 생성

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,14 @@
-const Button = ({ img_path }) => {
-  <button className="Button"></button>;
+import styled from "styled-components";
+
+const CustomButton = styled.button`
+  border: 0;
+  background-color: transparent;
+  font-weight: bolder;
+  font-size: 20px;
+`;
+
+const Button = ({ img_path, text }) => {
+  return <CustomButton>{text}</CustomButton>;
 };
 
 export default Button;
