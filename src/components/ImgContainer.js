61aed6a0a47e7d5536ea93d28e5a4afabad886b6
yunhwan <sky10240107@naver.com>
2023-04-06 22:52:36 +0900

    feat: ImgContainer 생성

---
+++ src/components/ImgContainer.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #88c9b9;
+    border-radius: 50%;
+    width: 350px;
+    height: 350px;
+  }
+
+  img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #88c9b9;
+    border-radius: 50%;
+    width: 350px;
+    height: 350px;
+    object-fit: contain;
+  }
+`;
+
+const ImgContainer = ({ img_path, size }) => {
+  return (
+    <ImgBox>
+      <img src={process.env.PUBLIC_URL + `/assets/Img3.png`} />
+    </ImgBox>
+  );
+};
+
+export default ImgContainer;
