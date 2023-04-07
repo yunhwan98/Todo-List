aa0989bbec429e8b5d09441cbbd298c54f7d7b3e
yunhwan <sky10240107@naver.com>
2023-04-07 21:43:26 +0900

    feat: ReadAll 페이지 구현

--- src/pages/ReadAll.js
+++ src/pages/ReadAll.js
@@ -1,7 +1,32 @@
-const ReadAll = () => {
+import styled from "styled-components";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import TodoList from "../components/ToDoList";
+
+const ReadAllPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #cbebd3;
+  border: black solid 1px;
+  height: 100vh;
+`;
+
+const ReadAll = ({ todo, removeTodo, updateTodo }) => {
   return (
     <>
-      <div>2</div>
+      <ReadAllPage>
+        <Header
+          headText={"All ToDo"}
+          leftChild={<Button text={"<"} />}
+          rightChild={<Button text={">"} />}
+        />
+        <TodoList
+          todo={todo}
+          removeTodo={removeTodo}
+          updateTodo={updateTodo}
+        ></TodoList>
+      </ReadAllPage>
     </>
   );
 };
